da82064d2a97f8332cd4284758c59513b6610312
Kai Strüssmann <kstrussmann@craftingit.de>
2019-10-12 16:30:25 +0200

    Push Dotnet Agent

--- .ci/docker_push_tag.sh
+++ .ci/docker_push_tag.sh
@@ -3,6 +3,7 @@ docker tag agent-docker craftingit/jenkins-agent-docker:$TRAVIS_TAG
 docker tag agent-javamaven craftingit/jenkins-agent-javamaven:$TRAVIS_TAG
 docker tag agent-nodejs craftingit/jenkins-agent-nodejs:$TRAVIS_TAG
 docker tag agent-yarn craftingit/jenkins-agent-yarn:$TRAVIS_TAG
+docker tag agent-dotnet craftingit/jenkins-agent-dotnet:$TRAVIS_TAG
 
 if [ ! "$TRAVIS_TAG" == "dev" ];
 then
@@ -10,6 +11,7 @@ then
     docker tag agent-javamaven craftingit/jenkins-agent-javamaven:latest
     docker tag agent-nodejs craftingit/jenkins-agent-nodejs:latest
     docker tag agent-yarn craftingit/jenkins-agent-yarn:latest
+    docker tag agent-dotnet craftingit/jenkins-agent-dotnet:latest
 fi
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
@@ -18,3 +20,4 @@ docker push craftingit/jenkins-agent-docker
 docker push craftingit/jenkins-agent-javamaven
 docker push craftingit/jenkins-agent-nodejs
 docker push craftingit/jenkins-agent-yarn
+docker push craftingit/jenkins-agent-dotnet
