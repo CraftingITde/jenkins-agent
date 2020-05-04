ebc23ecfab60ce5cfb2696958c3b1e6b555f5bca
Kai Strüssmann <kai.struessmann@timbertec.com>
2020-05-04 15:55:45 +0200

    Move to inbound-agent

--- .ci/updater.sh
+++ .ci/updater.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LATEST_VERSION=$(curl -s 'https://api.github.com/repos/jenkinsci/docker-jnlp-slave/releases/latest' | jq -r '.tag_name');
+LATEST_VERSION=$(curl -s 'https://api.github.com/repos/jenkinsci/docker-inbound-agent/releases/latest' | jq -r '.tag_name');
 CURRENT_VERSION=$(sed -nr 's/FROM jenkins\/jnlp-slave:*(.+)/\1/p' Container/docker/Dockerfile | tr -d '[:cntrl:]');
 REPO_ADRESS=$(git config --get remote.origin.url | sed 's~http[s]*://~~g')
 
