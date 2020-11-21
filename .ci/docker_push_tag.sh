77c35252eff3b38f7a499a4388b0461fb9111adf
Kai Strüssmann <kstruessmann@craftingit.de>
2020-11-21 10:16:27 +0100

    add dotnetblazor

--- .ci/docker_push_tag.sh
+++ .ci/docker_push_tag.sh
@@ -4,6 +4,7 @@ docker tag agent-javamaven craftingit/jenkins-agent-javamaven:$TRAVIS_TAG
 docker tag agent-javascript craftingit/jenkins-agent-javascript:$TRAVIS_TAG
 docker tag agent-dotnet craftingit/jenkins-agent-dotnet:$TRAVIS_TAG
 docker tag agent-releaser craftingit/jenkins-agent-releaser:$TRAVIS_TAG
+docker tag agent-dotnetblazor craftingit/jenkins-agent-dotnetblazor:$TRAVIS_TAG
 
 if [ ! "$TRAVIS_TAG" == "dev" ];
 then
@@ -12,6 +13,7 @@ then
     docker tag agent-javascript craftingit/jenkins-agent-javascript:latest
     docker tag agent-dotnet craftingit/jenkins-agent-dotnet:latest
     docker tag agent-releaser craftingit/jenkins-agent-releaser:latest
+    docker tag agent-dotnetblazor craftingit/jenkins-agent-dotnetblazor:latest
 fi
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
@@ -21,3 +23,4 @@ docker push craftingit/jenkins-agent-javamaven
 docker push craftingit/jenkins-agent-javascript
 docker push craftingit/jenkins-agent-dotnet
 docker push craftingit/jenkins-agent-releaser
+docker push craftingit/jenkins-agent-dotnetblazor
